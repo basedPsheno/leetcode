2610e26a7ca28e5dd22d2e78412353f6b5281750
basedPsheno <134151956+basedPsheno@users.noreply.github.com>
2023-05-21 17:29:48 +0300

    solved

---
+++ easy/richest_customer_wealth.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        for customer in accounts:
+            cur_wealth = 0
+            for wealth in customer:
+                cur_wealth += wealth
+            if cur_wealth > max_wealth:
+                max_wealth = cur_wealth
+        return max_wealth
