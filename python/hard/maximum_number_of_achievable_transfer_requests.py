0c8dd9ef2d08b3fca419564bfa41f80d88cde3a3
basedPsheno <134151956+basedPsheno@users.noreply.github.com>
2023-07-02 16:45:30 +0300

    solved

---
+++ python/hard/maximum_number_of_achievable_transfer_requests.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/
+
+import itertools
+
+class Solution:
+    def maximumRequests(self, n, req):
+        for k in range(len(req), 0, -1):
+            for c in itertools.combinations(range(len(req)), k):
+                degree = [0] * n
+                for i in c:
+                    degree[req[i][0]] -= 1
+                    degree[req[i][1]] += 1
+                if not any(degree):
+                    return k
+        return 0
