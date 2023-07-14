997613be5422c8d4505b723d3add96fc6f3c0ef4
basedPsheno <134151956+basedPsheno@users.noreply.github.com>
2023-07-14 16:59:01 +0300

    solved

---
+++ python/medium/longest_arithmetic_subsequence_of_given_difference.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        dp = {}
+        answer = 1
+        for a in arr:
+            before_a = dp.get(a - difference, 0)
+            dp[a] = before_a + 1
+            answer = max(answer, dp[a])
+            
+        return answer
